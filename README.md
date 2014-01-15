23d584721ce15757568d4683524ebca2d8bc6bc6
Sean Cummins <sean@scummins.com>
2014-01-15 16:00:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Parses EMC Symmetrix SYMCLI XML output (either online, or against an offline SYMAPIDB), and produces a report showing per-storage group capacity and parent->child relationship information.
---
+++ sgcapreport.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+sgcapreport.py - Reports capacity per Storage Group
+
+Requirements:
+- Python 2.7.x
+- prettytable -- "pip install prettytable" to install
+- EMC Solutions Enabler
+- SYMCLI bin directory in PATH
+
+"""
+
+import argparse
+import os
+import subprocess
+import re
+from prettytable import PrettyTable
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+### Define and Parse CLI arguments
+parser = argparse.ArgumentParser(description='Reports capacity per Symmetrix Storage Group.')
+rflags = parser.add_argument_group('Required arguments')
+rflags.add_argument('-sid',      required=True,help='Symmetrix serial number')
+rflags.add_argument('-offline',  required=True,help='Offline SYMAPIDB filename')
+sflags = parser.add_argument_group('Additional optional arguments')
+sflags.add_argument('-nochildren',         help='Flag; Skips reporting on Child Storage Groups',action="store_true")
+sflags.add_argument('-csv',                help='Flag; Outputs in CSV format',action="store_true")
+
+
+args = parser.parse_args()
+
+os.environ['SYMCLI_OFFLINE'] = '1'
+os.environ['SYMCLI_DB_FILE'] = os.path.abspath(args.symapi)
+
+### Capture SYMCLI TDEV information into ElementTree
+tdevcommand = "symcfg -sid " + args.sid + " list -tdev -gb -output xml_e"
+tdev_xml = subprocess.check_output(tdevcommand, shell=True)
+tdevtree = ET.fromstring(tdev_xml)
+
+### Put TDEV ElementTree values into Python data structure
+# tdevcap{ 'tdev1': [1024, 512, 512] }   # [total, allocated, written]
+tdevcap = dict()
+
+# Iterate through all TDEVs, capturing capacity information
+for elem in tdevtree.iterfind('Symmetrix/ThinDevs/Device'):
+    tdevcap[elem.find('dev_name').text] = [float(elem.find('total_tracks_gb').text), float(elem.find('alloc_tracks_gb').text), float(elem.find('written_tracks_gb').text)]
+
+
+### Capture SYMCLI SG information into ElementTree
+sgcommand = "symsg -sid " + args.sid + " list -v -output xml_e"
+symsg_xml = subprocess.check_output(sgcommand, shell=True)
+sgtree = ET.fromstring(symsg_xml)
+
+### Put SG ElementTree values into Python data structure
+# sgcapacity{ 'sg1': [1024, 512, 512] }   # [total, allocated, written]
+sgcapacity = dict()
+# sgparents { 'sg1': [ 'parentsg1', 'parentsg2', 'parentsg3']}
+sgparents = dict()
+# sgchildren{ 'sg1': [ 'childsg1', 'childsg2', 'childsg3']}
+sgchildren = dict()
+
+# Iterate through all SGs
+for elem in sgtree.iterfind('SG'):
+    sgname = elem.find('SG_Info/name').text
+
+
+    # Iterate through an SG's device members for members and their capacity
+    for member in elem.iterfind('DEVS_List/Device'):
+        membername = member.find('dev_name').text        
+        if membername in tdevcap:
+            devcapacity = tdevcap[membername]
+        else:
+            stdcap = float(member.find('megabytes').text)/1024
+            devcapacity = [stdcap,stdcap,stdcap]
+        if sgname not in sgcapacity:
+            # Create new dict key and list (i.e. we just found the meta head)
+            sgcapacity[sgname] = devcapacity
+        else:
+            # Append to existing key and list (i.e. we found a meta member)
+            sgcapacity[sgname] += devcapacity
+
+
+
+    # Iterate through an SG's SG members for Parent/Child information
+    for cascadesg in elem.iterfind('SG_Info/SG_group_info/SG'):
+        if cascadesg.find('Cascade_Status').text == "IsChild":
+            if sgname not in sgchildren:
+                sgchildren[sgname] = [cascadesg.find('name').text]
+            else:
+                sgchildren[sgname].append(cascadesg.find('name').text)
+        elif cascadesg.find('Cascade_Status').text == "IsParent":
+            if sgname not in sgparents:
+                sgparents[sgname] = [cascadesg.find('name').text]
+            else:
+                sgparents[sgname].append(cascadesg.find('name').text)
+
+
+
+# Build the PrettyTable
+if args.csv:
+    print "Storage Group,Total GB,Allocated GB,Written GB,Parents,Children"
+
+
+report = PrettyTable(["Storage Group","Total GB","Allocated GB","Written GB","Parents","Children"])
+for sg,mb in sgcapacity.items():
+    children,parents = ["",""]
+    if sg in sgchildren:
+        children = ",".join(sgchildren[sg])
+    if sg in sgparents:
+        if args.nochildren:
+            continue
+        else:
+            parents = ",".join(sgparents[sg])
+    if args.csv:
+        print ",".join([sg,str(sgcapacity[sg][0]),str(sgcapacity[sg][1]),str(sgcapacity[sg][2]),parents,children])
+    else:
+        report.add_row([sg,sgcapacity[sg][0],sgcapacity[sg][1],sgcapacity[sg][2],parents,children])
+
+
+# Format and print
+report.int_format = "10"
+report.float_format = "10.1"
+report.max_width["Children"] = 30
+report.format = True
+if not args.csv:
+    print report
+
+
+
